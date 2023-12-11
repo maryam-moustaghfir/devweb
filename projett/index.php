569ae4eb58de6a35ba0833fe374df5bd31626207
maryam-moustaghfir <139572680+maryam-moustaghfir@users.noreply.github.com>
2023-12-11 03:32:35 +0100

    Update index.php

--- projett/index.php
+++ projett/index.php
@@ -1,48 +1,13 @@
 <?php
-// Active le mode d'exception pour MySQLi
-mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-
-try {
-    // 1. on se connecte à MariaDB
-    $db = new mysqli('localhost', 'jeux-videos', 'IsImA_2023/%', 'jeux-videos', 3307);
-    $db->query('SET NAMES UTF8');
-
-    // 2. on exécute la requête
-    $sql = 'SELECT libelle FROM article';
-    $result = $db->query($sql);
-
-    // Vérifie si la requête s'est bien exécutée
-    if ($result === false) {
-        throw new Exception($db->error);
-    }
-
-    // 3. on fait une boucle pour lire chaque enregistrement
-    while ($data = $result->fetch_assoc()) {
-        // Affiche les données de chaque enregistrement
-        print_r($data);
-    }
-} catch (Exception $e) {
-    // Gère les exceptions, affiche l'erreur ou effectue d'autres actions nécessaires
-    echo 'Erreur : ' . $e->getMessage();
-} finally {
-    // 4. on ferme la connexion à MariaDB (dans le bloc finally pour s'assurer qu'elle est toujours fermée)
-    if ($db) {
-        $db->close();
-    }
-}
+require('fonctions.php');
+$connexion = connexion();
 ?>
-
-
 
 <html>
 	<head>
-		<!-- titre de la fenêtre -->
 		<title>Titre</title>
-		<!-- précise l'encodage au navigateur (gestion des accents, ...) -->
-		<meta http-equiv="Content-Type" content="text/html; charset= utf-8"/>
-		<!-- Feuille de style -->
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 		<link rel="stylesheet" type="text/css" href="styles.css">
-		<!-- Inibe la grande largeur sur mobile : évite que le mobile présente un écran large et qu'il faille zoomer -->
 		<meta name="viewport" content="initial-scale=1.0, maximum-scale=2.0" />
 	</head>
 	<body>
@@ -54,34 +19,66 @@ try {
 		<div class="authentification">
             <form>
                 <div class="authBloc">
-                <div class="container-left1">
-                <label for="email">adresse mail</label><br>
-</div>
-<div class="container-left2">
-
-                    <input type="email" name="email" /><br>
-</div>
                     <div class="container-left1">
-
-                    <label for="password">mot de passe</label><br>
-</div>
-<div class="container-left2">
-
-                    <input type="password" name="password" /><br>
+                        <label for="email">adresse mail</label><br>
+                    </div>
+                    <div class="container-left2">
+                        <input type="email" name="email" /><br>
+                    </div>
+                    <div class="container-left1">
+                        <label for="password">mot de passe</label><br>
+                    </div>
+                    <div class="container-left2">
+                        <input type="password" name="password" /><br>
+                    </div>
                 </div>
-</div>
                 <button class="bouton">s'inscrire</button>
                 <button class="bouton">connexion</button>                    
-            </form >
+            </form>
         </div>
-        <div class="contenu">
-    
-</div>
+        <div class="contenu">    
+        <?php
+        // Active le mode d'exception pour MySQLi
+        mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
+        try {
+            // Récupération de l'ID de la catégorie depuis l'URL
+            $categorie_id = isset($_GET['famille']) ? intval($_GET['famille']) : 0;
 
+            // Appel d'une fonction pour obtenir tous les articles de la catégorie
+            $articles = obtenirArticlesCategorie($connexion, $categorie_id);
 
+            // Vérifie si des articles ont été trouvés
+            if ($articles) {
+                echo '<div class="articles-container">';
+                // Affiche chaque article
+                foreach ($articles as $article) {
+                    echo '<div class="forme-container">';
 
+                    echo '<h1 class="libelle">' . $article['libelle'] . '</h1>';
+                    echo '<div class="detail">' . $article['detail'] .'...'. '</div>';
+                    echo '<p class="style-prix">' . $article['prix_ttc'] . ' €</p>';
+                    $imagePath = 'img_articles/' . $article['image'];
+                    echo '<img src="' . $imagePath . '" alt="Image de l\'article" class="img-article">';
+                    echo '<button class="bouton">'.'commander'.'</button>';
+                    echo '</div>';
+                }
+                echo '</div>'; // Fermez le conteneur de l'article
 
-		<div class="panier">panier</div>
+            } else {
+                echo 'Aucun article trouvé dans cette catégorie.';
+            }
+        } catch (Exception $e) {
+            // Gère les exceptions, affiche l'erreur ou effectue d'autres actions nécessaires
+            echo 'Erreur : ' . $e->getMessage();
+        } finally {
+            // Ferme la connexion à MariaDB
+            if ($connexion) {
+                $connexion->close();
+            }
+        }
+        ?>
+        </div>
+	<div class="panier">panier</div>
     </body>
 </html>
